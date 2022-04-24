535576d3b8b3c5f9996ba00aa308766fd4ac8fbc
Ondrej Bima <ondra@bimovi.cz>
2022-04-24 13:50:35 +0200

    fix TVRC_SamsungPre2016 on python3

--- resources/lib/tvrc_samsung_pre_2016.py
+++ resources/lib/tvrc_samsung_pre_2016.py
@@ -23,12 +23,12 @@ class TVRC_SamsungPre2016(BaseTVRemoteControl):
         sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sck.connect((self.host, 55000))
         msg = chr(0x64) + chr(0x00) +\
-            chr(len(base64.b64encode(self.app_id)))   + chr(0x00) + base64.b64encode(self.app_id) +\
-            chr(len(base64.b64encode(self.app_desc))) + chr(0x00) + base64.b64encode(self.app_desc) +\
-            chr(len(base64.b64encode(self.app_name))) + chr(0x00) + base64.b64encode(self.app_name)
+            chr(len(base64.b64encode(self.app_id.encode())))   + chr(0x00) + base64.b64encode(self.app_id.encode()).decode() +\
+            chr(len(base64.b64encode(self.app_desc.encode()))) + chr(0x00) + base64.b64encode(self.app_desc.encode()).decode() +\
+            chr(len(base64.b64encode(self.app_name.encode()))) + chr(0x00) + base64.b64encode(self.app_name.encode()).decode()
+        pkt = chr(0x00) * 3 + chr(len(msg)) + chr(0x00) + msg
+        sck.send(pkt.encode())
+        msg = chr(0x00) * 3 + chr(len(base64.b64encode(key.encode()))) + chr(0x00) + base64.b64encode(key.encode()).decode()
         pkt = chr(0x00) * 3 + chr(len(msg)) + chr(0x00) + msg  
-        sck.send(pkt)
-        msg = chr(0x00) * 3 + chr(len(base64.b64encode(key))) + chr(0x00) + base64.b64encode(key)
-        pkt = chr(0x00) * 3 + chr(len(msg)) + chr(0x00) + msg  
-        sck.send(pkt)
+        sck.send(pkt.encode())
         sck.close()
